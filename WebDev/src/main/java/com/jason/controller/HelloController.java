a58f5b2e9eb1a8aff875df8c80bd9ccdab90e65d
Jason <jasonliubob@gmail.com>
2016-05-05 21:22:46 +0800

    5.5

---
+++ WebDev/src/main/java/com/jason/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.jason.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	private static Logger log = LoggerFactory.getLogger(HelloController.class);
+	
+	@RequestMapping(value="/test",method=RequestMethod.GET)
+	public String test(){
+		log.info("hello method");
+		return "hello";
+	}
+}
